89d1f71891363ebe53c489c2d81b9640c98a7ed2
Conley Lee <conleylee@foxmail.com>
2025-07-29 19:58:06 +0800

    add cache

--- src/cache.hpp
+++ src/cache.hpp
@@ -1,3 +1,21 @@
 #ifndef __GLSLX_CACHE_HPP__
 #define __GLSLX_CACHE_HPP__
+
+#include <string>
+class Cache {
+    struct Record {
+        enum { VARIABLE, TYPE, FUNC, STRUCT } Kind;
+        std::string name;
+        std::string doc;
+        int start_line;
+		int end_line;
+        int start_col;
+		int end_col;
+        std::string filename;
+    };
+
+public:
+    Cache() = default;
+};
+
 #endif
